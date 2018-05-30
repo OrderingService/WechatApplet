decae18bede27e7ddbcccf71802ebff4100d444c
yyh15331030 <1019494590@qq.com>
2018-05-30 17:37:13 +0800

    V1.1.1

--- Ordering_Applet_v1.1.0/pages/index/index.js
+++ Ordering_Applet_v1.1.0/pages/index/index.js
@@ -65,77 +65,92 @@ Page({
           {
             "image_url": "../../images/noodles/noodles1.jpg",
             "name": "抹茶海鲜杯面",
-            "price": 12
+            "price": 12,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles2.jpg",
             "name": "林氏盖饭杯面",
-            "price": 11
+            "price": 11,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles3.jpg",
             "name": "维他命b1脑面",
-            "price": 14
+            "price": 14,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles4.jpg",
             "name": "饺子重击面",
-            "price": 12
+            "price": 12,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles5.jpg",
             "name": "泰式绿咖喱面",
-            "price": 9
+            "price": 9,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles6.jpg",
             "name": "明太子乌冬面",
-            "price": 15
+            "price": 15,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles7.jpg",
             "name": "培根蛋酱风味乌冬面",
-            "price": 13
+            "price": 13,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles8.jpg",
             "name": "意面杯面",
-            "price": 11
+            "price": 11,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles9.jpg",
             "name": "酸奶咖喱面",
-            "price": 12
+            "price": 12,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles10.jpg",
             "name": "菠萝拉面",
-            "price": 12
+            "price": 12,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles11.jpg",
             "name": "巧克力炒面",
-            "price": 13
+            "price": 13,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles12.jpg",
             "name": "草莓蛋糕炒面",
-            "price": 15
+            "price": 15,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles13.jpg",
             "name": "芥末蛋黄酱炒面",
-            "price": 12
+            "price": 12,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles14.jpg",
             "name": "香菜炒面",
-            "price": 10
+            "price": 10,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/noodles/noodles15.jpg",
             "name": "纳豆炒面",
-            "price": 11
+            "price": 11,
+            "buy_num": 0
           }
         ]
       },
@@ -145,27 +160,32 @@ Page({
           {
             "image_url": "../../images/foods/appetizer1.jpg",
             "name": "鸡肉芝士凯撒沙拉",
-            "price": 10
+            "price": 10,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/appetizer2.jpg",
             "name": "炸鸡软骨",
-            "price": 7
+            "price": 7,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/appetizer3.jpg",
             "name": "牛油焗扇贝",
-            "price": 14
+            "price": 14,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/appetizer4.jpg",
             "name": "烤鱿鱼",
-            "price": 12
+            "price": 12,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/appetizer5.jpg",
             "name": "和风比萨(照烧鸡)",
-            "price": 28
+            "price": 28,
+            "buy_num": 0
           }
         ]
       },
@@ -175,37 +195,44 @@ Page({
           {
             "image_url": "../../images/foods/entree1.jpg",
             "name": "牛肉锅烧乌冬",
-            "price": 12
+            "price": 12,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/entree2.jpg",
             "name": "广岛风铁板炒面",
-            "price": 11
+            "price": 11,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/entree3.jpg",
             "name": "石锅猪肉饭",
-            "price": 14
+            "price": 14,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/entree4.jpg",
             "name": "猪骨汤拉面",
-            "price": 12
+            "price": 12,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/entree5.jpg",
             "name": "特盛牛肉饭",
-            "price": 13
+            "price": 13,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/entree6.jpg",
             "name": "鲜味虾煲",
-            "price": 13
+            "price": 13,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/entree7.jpg",
             "name": "牛腩咖喱",
-            "price": 13
+            "price": 13,
+            "buy_num": 0
           }
         ]
       },
@@ -215,37 +242,44 @@ Page({
           {
             "image_url": "../../images/foods/dessert1.jpg",
             "name": "软雪布丁",
-            "price": 8
+            "price": 8,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/dessert2.jpg",
             "name": "抹茶雪糕",
-            "price": 5
+            "price": 5,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/dessert3.jpg",
             "name": "法芙娜巧克力松饼",
-            "price": 12
+            "price": 12,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/dessert4.jpg",
             "name": "芒果鸡肉三明治",
-            "price": 8
+            "price": 8,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/dessert5.jpg",
             "name": "纸杯蛋糕",
-            "price": 5
+            "price": 5,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/dessert6.jpg",
             "name": "百香果汁",
-            "price": 7
+            "price": 7,
+            "buy_num": 0
           },
           {
             "image_url": "../../images/foods/dessert7.jpg",
             "name": "草莓奶茶",
-            "price": 10
+            "price": 10,
+            "buy_num": 0
           }
         ]
       }],
@@ -268,24 +302,30 @@ Page({
     var index = e.currentTarget.dataset.index;
     var isInCart = this.data.isInCart;
     var cartList = this.data.cartList;
+    var listData = this.data.listData;
     var item = {
       "index": 15 * type + index,
       "name": this.data.listData[type].foods[index].name,
       "price": this.data.listData[type].foods[index].price,
-      "number": 1,
+      "number": 1
     };
+   
     if (isInCart[item.index]) {
       for (var i in cartList) {
         if (cartList[i].name == item.name) {
           cartList[i].number++;
+          this.data.listData[type].foods[index].buy_num = cartList[i].number;
           break;
         }
       }
     } else {
       cartList.push(item);
       isInCart[item.index] = true;
+      this.data.listData[type].foods[index].buy_num = 1;
     }
+
     this.setData({
+      listData: listData,
       cartList: cartList,
       isInCart: isInCart,
       sumMoney: this.data.sumMoney + item.price,
@@ -300,6 +340,7 @@ Page({
     var index = e.currentTarget.dataset.index;
     var isInCart = this.data.isInCart;
     var cartList = this.data.cartList;
+    var listData = this.data.listData;
     var item = {
       "index": 15 * type + index,
       "name": this.data.listData[type].foods[index].name,
@@ -311,14 +352,18 @@ Page({
       if (cartList[i].name == item.name) {
         if (cartList[i].number != 1) {
           cartList[i].number--;
+          this.data.listData[type].foods[index].buy_num = cartList[i].number;
         } else {
           cartList.splice(i, 1);
           isInCart[item.index] = false;
+          this.data.listData[type].foods[index].buy_num = 0;
         }
         break;
       }
     }
+    
     this.setData({
+      listData: listData,
       cartList: cartList,
       isInCart: isInCart,
       sumMoney: this.data.sumMoney - item.price,
@@ -350,8 +395,18 @@ Page({
   addNumber: function (e) {
     var index = e.currentTarget.dataset.index;
     var cartList = this.data.cartList;
+    var listData = this.data.listData;
     cartList[index].number++;
+    for (var i in listData) {
+      for (var j in listData[i].foods) {
+        if (listData[i].foods[j].name == cartList[index].name) {
+          listData[i].foods[j].buy_num = cartList[index].number;
+          break;
+        }
+      }
+    }
     this.setData({
+      listData: listData,
       cartList: cartList,
       sumMoney: this.data.sumMoney + cartList[index].price,
       foodNumber: this.data.foodNumber + 1
@@ -362,14 +417,32 @@ Page({
     var index = e.currentTarget.dataset.index;
     var isInCart = this.data.isInCart;
     var cartList = this.data.cartList;
+    var listData = this.data.listData;
     var sumMoney = this.data.sumMoney - cartList[index].price;
     if (cartList[index].number == 1) {
       isInCart[cartList[index].index] = false;
+      for (var i in listData) {
+        for (var j in listData[i].foods) {
+          if (listData[i].foods[j].name == cartList[index].name) {
+            listData[i].foods[j].buy_num = 0;
+            break;
+          }
+        }
+      }
       cartList.splice(index, 1);
     } else {
       cartList[index].number--;
+      for (var i in listData) {
+        for (var j in listData[i].foods) {
+          if (listData[i].foods[j].name == cartList[index].name) {
+            listData[i].foods[j].buy_num = cartList[index].number;
+            break;
+          }
+        }
+      }
     }
     this.setData({
+      listData: listData,
       cartList: cartList,
       isInCart: isInCart,
       sumMoney: sumMoney,
